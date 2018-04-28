76a13c08366f264f811453d1a4a9d75890d33f4e
k0nnie <khoessel@web.de>
2018-04-28 21:23:24 +0200

    introduce_new_test_classes - introduce empty test classes for Tui & Controller

---
+++ src/test/scala/de/htwg/se/mastermind/aview/TuiSpec.scala
@@ -0,0 +1,10 @@
+package de.htwg.se.mastermind.aview
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.{Matchers, WordSpec}
+
+@RunWith(classOf[JUnitRunner])
+class TuiSpec extends WordSpec with Matchers {
+
+}
