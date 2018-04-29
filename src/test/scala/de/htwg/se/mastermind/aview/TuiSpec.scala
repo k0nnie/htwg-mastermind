f2cfdc45f3a12a1525180a866cf613205718f728
k0nnie <khoessel@web.de>
2018-04-29 12:15:42 +0200

    observer_pattern - testing

--- src/test/scala/de/htwg/se/mastermind/aview/TuiSpec.scala
+++ src/test/scala/de/htwg/se/mastermind/aview/TuiSpec.scala
@@ -1,10 +1,23 @@
 package de.htwg.se.mastermind.aview
 
+import de.htwg.se.mastermind.controller.Controller
+import de.htwg.se.mastermind.model.Board
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.{Matchers, WordSpec}
 
 @RunWith(classOf[JUnitRunner])
 class TuiSpec extends WordSpec with Matchers {
-
+  "A Mastermind Tui" should {
+    val controller = new Controller(new Board())
+    val tui = new Tui(controller)
+    "create and empty Mastermind on input 'n'" in {
+      tui.processInputLine("n", 0)
+      controller.board.rounds should be(new Board().rounds)
+    }
+    "set a turn on input 'rgbp'" in {
+      tui.processInputLine("123", 0)
+      controller.board.rounds(0).turn.pegString should be("Vector( ,  ,  ,  )")
+    }
+  }
 }
--- src/test/scala/de/htwg/se/mastermind/controller/mastermind/ControllerSpec.scala
+++ src/test/scala/de/htwg/se/mastermind/controller/mastermind/ControllerSpec.scala
@@ -1,10 +1,38 @@
 package de.htwg.se.mastermind.controller.mastermind
 
+import de.htwg.se.mastermind.controller.Controller
+import de.htwg.se.mastermind.model.{Board, Color}
+import de.htwg.se.mastermind.util.Observer
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.{Matchers, WordSpec}
 
 @RunWith(classOf[JUnitRunner])
 class ControllerSpec extends WordSpec with Matchers {
+  "A Controller" when {
+    "observed by an Observer" should {
+      val board = new Board()
+      val controller = new Controller(board)
+      val observer = new Observer {
+        var updated: Boolean = false
 
+        def isUpdated: Boolean = updated
+
+        override def update: Unit = updated = true
+
+      }
+      controller.add(observer)
+      "notify its Observer after creation" in {
+        controller.createEmptyBoard()
+        observer.updated should be(true)
+        controller.board.rounds.size should be(8)
+      }
+      "notify its Observer after replacing a round" in {
+        val colVec = Vector[Color](Color("r"), Color("r"), Color("r"), Color("r"))
+        controller.replaceRound(0, colVec)
+        observer.updated should be(true)
+        controller.board.rounds(0).turn.pegString should be("Vector(r, r, r, r)")
+      }
+    }
+  }
 }
