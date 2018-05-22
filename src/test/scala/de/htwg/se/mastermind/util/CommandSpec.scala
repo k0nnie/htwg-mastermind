494d05b3a705ee84a702a988c943074ed320dfb0
k0nnie <Skl!m6))>
2018-05-22 12:40:39 +0200

    Issue #22 - code formatting

--- src/test/scala/de/htwg/se/mastermind/util/CommandSpec.scala
+++ src/test/scala/de/htwg/se/mastermind/util/CommandSpec.scala
@@ -5,14 +5,14 @@ import org.scalatest.{Matchers, WordSpec}
 import org.scalatest.junit.JUnitRunner
 
 class incrCommand extends Command {
-  var state:Int =0
-  override def doStep(): Unit = state+=1
+  var state: Int = 0
 
-  override def undoStep(): Unit = state-=1
+  override def doStep(): Unit = state += 1
 
-  override def redoStep(): Unit = state+=1
+  override def undoStep(): Unit = state -= 1
+
+  override def redoStep(): Unit = state += 1
 }
-
 
 @RunWith(classOf[JUnitRunner])
 class CommandSpec extends WordSpec with Matchers {
